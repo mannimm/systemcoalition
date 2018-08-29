d4c1d3020cd9c5f96a646cec97e657b35ea94866
Antonio David <nvncbldotcom@gmail.com>
2018-08-28 23:23:34 -0500

    more updates

--- skin/frontend/rwd/systemcoalition/js/script.js
+++ skin/frontend/rwd/systemcoalition/js/script.js
@@ -1,5 +1,6 @@
 function initNavigation(){
 	jQuery( '.main-container' ).css( 'padding-top', jQuery('#header').outerHeight() +'px' );
 }
+jQuery( document ).ready( initNavigation );
 jQuery( window ).scroll( initNavigation );
 jQuery( window ).resize( initNavigation );
